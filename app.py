9bdf2f8b03df122e63a485e0cb385a4df06c8e83
Jawahar lakshmanan <52790465+jawaluke@users.noreply.github.com>
2020-07-15 19:03:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ Enter the **Movie Name** see the result of the movie recommendation""")
 
     option = st.text_input(" which movie do you like the most? ")
     
-    if df["title"].isin([option.lower()]):
+    if df["title"].lower()==option.lower():
         st.write("you selected : ",option)
 
         result_movie = search_movie(option.lower())
